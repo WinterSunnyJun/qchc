4da4ad8e9733248f45da260b14cb614e2555ce35
WinterSunnyJun <496900910@qq.com>
2017-10-19 19:04:45 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -4,4 +4,14 @@ $(function () {
         anchors:['page1','page2','page3','page4','page5'],
         menu: '#menu'
     });
+//NAV
+    $(".nav li").click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+
+    $(".fwxm-btn li").click(function () {
+        var fwNow = $(this).index();
+        $(".fwxm-btn li").eq(fwNow).addClass('active').siblings().removeClass('active');
+        $(".fwxm-box > li").eq(fwNow).show().siblings().hide();
+    })
 })
