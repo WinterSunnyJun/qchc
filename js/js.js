16e67dc643aef67378acddd271f3ff3e9a72d01c
WinterSunnyJun <496900910@qq.com>
2017-10-20 15:52:59 +0800

    ajun

--- js/js.js
+++ js/js.js
@@ -8,10 +8,42 @@ $(function () {
     $(".nav li").click(function () {
         $(this).addClass('active').siblings().removeClass('active');
     })
-
+    //服务项目
     $(".fwxm-btn li").click(function () {
         var fwNow = $(this).index();
         $(".fwxm-btn li").eq(fwNow).addClass('active').siblings().removeClass('active');
         $(".fwxm-box > li").eq(fwNow).show().siblings().hide();
+    })
+//  案例详情
+    var axNow = 0;
+    $(".alxq-left").click(function () {
+        axNow--;
+        if(axNow < 0 ){
+            axNow = $(".alxq-item-picbg ul li").length-1;
+        }
+        $(".alxq-item-picbg ul li").eq(axNow).show().siblings().hide();
+    })
+    $(".alxq-right").click(function () {
+        axNow++;
+        if(axNow > $(".alxq-item-picbg ul li").length-1){
+            axNow = 0;
+        }
+        $(".alxq-item-picbg ul li").eq(axNow).show().siblings().hide();
+    })
+//    关于我们
+    var auNow = 0;
+    $(".gywm-left").click(function () {
+        auNow--;
+        if(auNow < 0 ){
+            auNow = $(".gywm-list li").length-1;
+        }
+        $(".gywm-list li").eq(auNow).show().siblings().hide();
+    })
+    $(".gywm-right").click(function () {
+        auNow++;
+        if(auNow > $(".gywm-list li").length-1){
+            auNow = 0;
+        }
+        $(".gywm-list li").eq(auNow).show().siblings().hide();
     })
 })
